f427e1f94ff889788195a4c884178b4d02e2188e
riya <thangababu@gmail.com>
2017-05-31 22:07:29 -0500

    More commit

--- src/C++Tests.cpp
+++ src/C++Tests.cpp
@@ -7,9 +7,24 @@
 //============================================================================
 
 #include <iostream>
+#include <memory>
 using namespace std;
 
+typedef std::unique_ptr<int> IntPtr;
+
+void passUniquePtr(IntPtr &x)
+{
+	//auto y =x;
+}
+
+
 int main() {
+	IntPtr up = std::make_unique<int>(1298);
+	//cout << up. << "--> Is the Unique ptr content" << endl;
+	passUniquePtr(up);
+	IntPtr up2;
+	up2=up;
+	cout << up.get() << "--> Is the Unique ptr content with get " << endl;
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
